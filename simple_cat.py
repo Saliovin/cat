5c49927bb32c6a030f505924bf3a623c62c1a55d
Jonas <jonas.eukan@gmail.com>
2019-10-02 14:03:10 +0800

    Refactored simple_cat.py

--- simple_cat.py
+++ simple_cat.py
@@ -1,40 +1,85 @@
-# From a list of files, this function reads each file and
-# combines them in a single string and returns the string
+import argparse
+
+
 def get_data(files):
+    """
+    Return a string containing the combined content of all the files in the list parameter.
+
+    :param files: A list of file names in the form of strings.
+    :return: a string containing the combined content of all the files in the list parameter.
+    """
     read_string = ""
     for file in files:
         with open(file, "r") as f:
             read_string += f.read()
-    return(read_string)
+    return read_string
 
-# This function accepts multiple file arguments and prints
-# the contents of each file.
-def read(*files):
+
+def read(files):
+    """
+    Print the combined contents of all the files in the list parameter.
+
+    :param files: A list of file names in the form of strings.
+    :return: None
+    """
     print(get_data(files))
 
-# This function accepts the first argument as the output file
-# and the following arguments as files to be concatenated.
-# Using this function will concatenate the passed files and
-# output them to the output file. If the output file is missing,
-# a new file will be created, else the existing output file will
-# be overwritten.
-def concat(output_file, *files):
+
+def concat(output_file, files):
+    """
+    Concatenate all files except the last one in the list and output it to the last file in the list.
+
+    :param output_file: Target file to output the concatenation.
+    :param files: A list of file names in the form of strings.
+    :return: None
+    """
     string_to_concat = get_data(files)
     with open(output_file, "w+") as f:
-        f.write(string_to_concat)
+        return f.write(string_to_concat)
 
-# This function accepts the first argument as the output file
-# and the following arguments as files to be appended.
-# Using this function will append the passed files to the
-# output file. If the output file is missing, a new file will
-# be created.
-def append(output_file, *files):
+
+def append(output_file, files):
+    """
+    Combine the contents of all files except the last one in the list and append it to the last file in the list.
+
+    :param output_file: Target file to append to.
+    :param files: A list of file names in the form of strings
+    :return: None
+    """
     string_to_append = get_data(files)
     with open(output_file, "a+") as f:
         f.write(string_to_append)
 
-# This function creates a new file using the filename given.
-# Does nothing if there is already an existing file.
-def create(filename):
-    open(filename, "w+")
-    filename.close()
+
+def create(filenames):
+    """
+    Create non-existing files in the given list parameter.
+
+    :param filenames: A list of file names in the form of strings
+    :return: None
+    """
+    for filename in filenames:
+        f = open(filename, "w+")
+        f.close()
+
+
+# Adding arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("files", nargs="*", help="Files to be opened. Prints the contents of each file if there are no"
+                                             "optional arguments.")
+parser.add_argument("-a", "--append", action="store_true", help="Takes all the data from inputted files, except the "
+                                                                "last and appends them to the last file. Creates a new"
+                                                                " file if output file is non-existing.")
+parser.add_argument("-c", "--concat", action="store_true", help="Takes all the data from inputted files, except the "
+                                                                "last and concatenates them to the last file. Creates a"
+                                                                " new file if output file is non-existing.")
+parser.add_argument("-mk", "--make", action="store_true", help="Creates each inputted file if non-existing.")
+args = parser.parse_args()
+if args.append:
+    append(args.files[-1], args.files[:-1])
+elif args.concat:
+    concat(args.files[-1], args.files[:-1])
+elif args.make:
+    create(args.files)
+else:
+    read(args.files)
